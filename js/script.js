41e4b52b4a13a0f030818fea8ac9ae73151febcb
al-xndram <amaftei@ocadu.ca>
2024-03-27 10:48:35 -0400

    most recent

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-var elements = $('.thing');
 
+var elements = $('.click');
+var z = 9999
 console.log(elements);
 
 elements.click((e)=> console.log(e))
 
 $(elements).click(function(){
-    $('.thing').each(function(index, element) {
+    $('.click').each(function(index, element) {
         console.log(element.style.zIndex)
         let layer = element.style.zIndex
         element.style.zIndex = layer + 1
     })
-    $(this).css("z-index", "0")
+
+    z=z-1;
+
+    $(this).css("z-index", z)
     // $(this).css("background", "white")
 
 
