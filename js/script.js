0db744ac80f90195b7de58b07a87267a39c1c958
al-xndram <amaftei@ocadu.ca>
2024-04-16 03:45:07 -0400

    it is complete

--- js/script.js
+++ js/script.js
@@ -38,3 +38,35 @@ $(document).ready(function() {
         $('#clickSound')[0].play(); 
     });
 });
+
+$(document).ready(function() {
+    // Check if the page is main.html and play the audio "glitter"
+    if (window.location.pathname.includes("main.html")) {
+        $('#glitter')[0].currentTime = 0;
+        $('#glitter')[0].play(); 
+    }
+});
+
+
+
+
+// Trying to get the click sound on the landing page...
+
+$(document).ready(function() {
+    // Event listener for click event on the link
+    $('#button').click(function(event) {
+        // Prevent default link behavior
+        event.preventDefault();
+        
+        // Play the click sound
+        $('#clickSound')[0].currentTime = 0; // Reset audio to start
+        $('#clickSound')[0].play();
+        
+        // Optionally, perform other actions after playing the sound
+        // For example, navigate to the link target after a delay
+        var targetUrl = $(this).attr('href');
+        setTimeout(function() {
+            window.location.href = targetUrl;
+        }, 5000); // Navigate after 500 milliseconds (adjust as needed)
+    });
+});
